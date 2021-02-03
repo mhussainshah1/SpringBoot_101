c44a07aeb01f9dc6a0fb8581e8698f633988c0da
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-02-03 00:54:00 -0500

    Java 15

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "index";
     }
 }
